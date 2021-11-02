77c145c7aea0f81939a174ed643f7093999654b0
apostol1322 <tolys2013@hotmail.gr>
2021-11-02 06:22:11 +0200

    b

--- connection.php
+++ connection.php
@@ -1,14 +1,20 @@
 <?php
-$servername = "sql11.freemysqlhosting.net";
-$username = "sql11435752";
-$password = "8kxebkB8gM";
+  /*$servername = "sql11.freemysqlhosting.net";
+  $username = "sql11435752";
+  $password = "8kxebkB8gM";*/
+  define('servername', 'sql11.freemysqlhosting.net');
+  define('username', 'sql11435752');
+  define('password', '8kxebkB8gM');
+  define('dbname', 'login');
 
-// Create connection
-$conn = mysqli_connect($servername, $username, $password);
+  // Create connection
+  $conn = mysqli_connect(servername, username, password, dbname);
 
-// Check connection
-if (!$conn) {
-  die("Connection failed: " . mysqli_connect_error());
-}
-echo "Connected successfully";
+  // Check connection
+  if ($conn == false) {
+    die("Connection failed: " . mysqli_connect_error());
+  }
+  else{
+    echo "Connected successfully";
+  }
 ?>
--- login.php
+++ login.php
@@ -1,16 +1,17 @@
 <?php
-// define variables and set to empty values
-$name = $email = $gender = $comment = $website = "";
+    require_once "connection.php";
+    // define variables and set to empty values
+    $name = $password = "";
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  $name = test_input($_POST["name"]);
-  $email = test_input($_POST["email"]);
-}
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+      $name = test_input($_POST["name"]);
+      $email = test_input($_POST["password"]);
+    }
 
-function test_input($data) {
-  $data = trim($data);
-  $data = stripslashes($data);
-  $data = htmlspecialchars($data);
-  return $data;
-}
+    function test_input($data) {
+      $data = trim($data);
+      $data = stripslashes($data);
+      $data = htmlspecialchars($data);
+      return $data;
+    }
 ?>
